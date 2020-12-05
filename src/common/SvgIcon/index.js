59b2794bbd233192439f54038b5bda75875f5fc0
thai.ly <thai.ly@masterisehomes.com>
2020-12-05 11:12:14 +0700

    update

--- src/common/SvgIcon/index.js
+++ src/common/SvgIcon/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactSVG } from "react-svg";
 
-const SvgIcon = (props) => <ReactSVG {...props} src={`/img/svg/${props.src}`} />;
+const SvgIcon = (props) => <ReactSVG {...props} src={`my-profile/img/svg/${props.src}`} />;
 
 export default SvgIcon;
